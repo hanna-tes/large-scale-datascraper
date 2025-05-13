1bc8364f12de385beab21622144c37b90323661f
hanna <82633682+hanna-tes@users.noreply.github.com>
2025-05-13 15:43:08 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-# Install Google Chrome
-apt-get update
-apt-get install -y wget unzip curl gnupg
+# Install dependencies
+apt-get update && apt-get install -y \
+    wget \
+    curl \
+    unzip \
+    gnupg \
+    libnss3 \
+    libxss1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libgbm1 \
+    libgtk-3-0 \
+    fonts-liberation \
+    libappindicator3-1 \
+    xdg-utils \
+    --no-install-recommends
+
+# Install Google Chrome (stable)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
