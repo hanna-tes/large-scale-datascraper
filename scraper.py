29b39f9e5024cf51640998398344129726f182ad
hanna <82633682+hanna-tes@users.noreply.github.com>
2025-05-13 15:12:31 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,11 +22,13 @@ warnings.filterwarnings('ignore')
 
 @st.cache_resource
 def get_driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+    service = ChromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
 def scrape_user_topics(driver, username):
